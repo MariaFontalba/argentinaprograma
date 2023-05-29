74b4c2167979d448088ccbdaf84e05b0c28e8097
MariaFontalba <mfontalba4043@gmail.com>
2023-05-29 09:49:25 -0300

    Practico NO finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,14 @@
+let baseRectangulo1 = 5;
+let alturaRectangulo1 = 3;
+let areaRectangulo1 = baseRectangulo1 * alturaRectangulo1;
+console.log (areaRectangulo1)
+
+//El area es de 15 cm 
+
+
+let baseRectangulo2 = 12.5;
+let alturaRectangulo2 = 5.3;
+let areaRectangulo2 = baseRectangulo2 * alturaRectangulo2;
+console.log(areaRectangulo2)
+
+//El area es de 66.25
---
+++ practico0/promedio3.js
@@ -0,0 +1,29 @@
+let x = 5;
+let y = 7;
+let z = 9;
+let promedio = (x + y+ z) / 3;
+console.log(promedio)
+
+// promedio es de 7
+
+x = -2
+y = -3
+z = -8
+promedio = (x + y+ z) / 3;
+console.log (promedio)
+
+//promedio es de -4.33333
+
+x = -1.2
+y = 5.3
+z = -3.3
+promedio = (x + y+ z) / 3;
+console.log (promedio)
+
+//promedio es de 0.266666
+
+
+
+
+
+
