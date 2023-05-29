bfee876a39ff9ff1692ce9e8627f9c05c1b4b741
MariaFontalba <mfontalba4043@gmail.com>
2023-05-29 09:47:42 -0300

    Agrego el archivo conversionGrados.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,12 @@
+let gradosFahrenheit = 50;
+let gradosCelsius = (gradosFahrenheit -32) * 5 / 9;
+console.log (gradosCelsius)
+
+//50 grados fahrenheit son 10 grados celsius
+
+
+gradosFahrenheit = 75;
+gradosCelsius = (gradosFahrenheit -32) * 5 / 9;
+console.log (gradosCelsius)
+
+//75 grados fahrenheit son 23.8 grados celsius
