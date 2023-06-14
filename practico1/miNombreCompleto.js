267bc6360c0fa31b2c3ee8265f016bb1196861a0
MariaFontalba <mfontalba4043@gmail.com>
2023-06-13 21:38:29 -0300

    guardo Nombre completo

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,7 @@
+let nombre
+nombre = "Maria" 
+let apellido = "Fontalba" 
+let edad = 37
+let miNombreCompleto = "Hola, " + nombre + " " + apellido + "." + " Tienes " + edad + " años.";
+
+console.log(miNombreCompleto)
