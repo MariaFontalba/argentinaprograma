f89cb9945e34393042309e1ec714ee62e7505753
MariaFontalba <mfontalba4043@gmail.com>
2023-06-13 22:18:37 -0300

    guardo ejercic. manejo de arreglos

---
+++ practico1/frutas.js
@@ -0,0 +1,23 @@
+//ejercicios a) y b)//
+
+let frutas = []
+
+frutas.push("banana", "manzana", "durazno", "naranja", "mandarina");
+
+console.log(frutas);
+
+
+//ejercicio c)//
+
+
+let ultima = frutas.pop();
+
+console.log("La ultima fruta agregada fue: " + ultima)
+
+
+//ejercicio d)//
+
+
+let primera = frutas.shift();
+
+console.log("la primera fruta agregada fue: " + primera)
