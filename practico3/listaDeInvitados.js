999467da1ddd8ad2517c33382dd296eeaf11510a
MariaFontalba <mfontalba4043@gmail.com>
2023-06-26 19:04:27 -0300

    Segundo Ejercicio

---
+++ practico3/listaDeInvitados.js
@@ -0,0 +1,49 @@
+let admitidos = [];
+
+let rechazados = [];
+
+let personas = [
+  "Jose",
+  "Maria",
+  "Juan",
+  "Andres",
+  "Lionel",
+  "Sofia",
+  "Leandro",
+  "Emilia",
+];
+
+for (let i = 0; i < personas.length; i++) {
+  if (personas[i] == "Jose" || personas[i] == "Sofia") {
+    rechazados.push(personas[i]);
+  } else {
+    admitidos.push(personas[i]);
+  }
+}
+
+console.log("La lista de invitados admitidos es:");
+
+for (let i = 0; i < admitidos.length; i++) {
+  console.log(admitidos[i]);
+}
+
+console.log("La lista de invitados rechazados es:");
+
+for (let i = 0; i < rechazados.length; i++) {
+  console.log(rechazados[i]);
+}
+
+
+
+
+
+
+//Ejercicio Extra//
+
+console.log("La lista ordenada de invitados admitidos es:");
+
+console.log(admitidos.sort());
+
+console.log("La lista ordenada de invitados rechazados es:");
+
+console.log(rechazados.sort());
